8500eced9dc98768d2431ee72c0f7e0974735ae2
Владислав <79568886+V-MilkyWay@users.noreply.github.com>
2022-11-02 14:53:48 +0300

    Add files via upload

---
+++ ResponsMap/index.js
@@ -0,0 +1,9 @@
+let menu = document.querySelectorAll('.navigation')
+
+   function openMenu(){
+    menu[0].style.display = 'flex';
+   }
+
+   function clouseMenu(){
+    menu[0].style.display = 'none';
+   }
