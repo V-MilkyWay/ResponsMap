4c9382296f6ca250ef6ccf4be50b3b8e725d694e
Владислав <79568886+V-MilkyWay@users.noreply.github.com>
2022-11-21 17:16:03 +0300

    Add files via upload

--- ResponsMap/index.js
+++ ResponsMap/index.js
@@ -1,4 +1,12 @@
-let menu = document.querySelectorAll('.navigation')
+let menu = document.querySelectorAll('.navigation');
+let mapRoads = document.querySelectorAll('.map-road');
+let mapPublic = document.querySelectorAll('.map-people');
+let mapWater = document.querySelectorAll('.map-river');
+let mapCity = document.querySelectorAll('.map-city');
+let buttonMapRoads = document.querySelectorAll('.filter_image-road');
+let buttonMapPublic = document.querySelectorAll('.filter_image-river');
+let buttonMapWater = document.querySelectorAll('.filter_image-people');
+let buttonMapCity = document.querySelectorAll('.filter_image-city');
 
    function openMenu(){
     menu[0].style.display = 'flex';
@@ -6,4 +14,48 @@ let menu = document.querySelectorAll('.navigation')
 
    function clouseMenu(){
     menu[0].style.display = 'none';
+   }
+
+   function openMapRoads() {
+      mapRoads[0].style.display = 'flex';
+      mapPublic[0].style.display = 'none';
+      mapWater[0].style.display = 'none';
+      mapCity[0].style.display = 'none';
+      buttonMapRoads[0].src = './image/road.png';
+      buttonMapPublic[0].src = './image/people-unactive.png';
+      buttonMapWater[0].src = './image/water-unactive.png';
+      buttonMapCity[0].src = './image/city-unactive.png';
+   }
+
+   function openMapPublic() {
+      mapPublic[0].style.display = 'flex';
+      mapRoads[0].style.display = 'none';
+      mapWater[0].style.display = 'none';
+      mapCity[0].style.display = 'none';
+      buttonMapRoads[0].src = './image/road-unactive.png';
+      buttonMapPublic[0].src = './image/people1.png';
+      buttonMapWater[0].src = './image/water-unactive.png';
+      buttonMapCity[0].src = './image/city-unactive.png';
+   }
+
+   function openMapWater() {
+      mapWater[0].style.display = 'flex';
+      mapRoads[0].style.display = 'none';
+      mapPublic[0].style.display = 'none';
+      mapCity[0].style.display = 'none';
+      buttonMapRoads[0].src = './image/road-unactive.png';
+      buttonMapPublic[0].src = './image/people-unactive.png';
+      buttonMapWater[0].src = './image/river.png';
+      buttonMapCity[0].src = './image/city-unactive.png';
+   }
+
+   function openMapCity() {
+      // mapCity[0].style.display = 'flex';
+      // mapRoads[0].style.display = 'none';
+      // mapPublic[0].style.display = 'none';
+      // mapWater[0].style.display = 'none';
+      //buttonMapRoads[0].src = './image/road-unactive.png';
+      //buttonMapPublic[0].src = './image/people-unactive.png';
+      //buttonMapWater[0].src = './image/water-unactive.png';
+      //buttonMapCity[0].src = './image/city.png';
    }
